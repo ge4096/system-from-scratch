da869784cbe2c2506db2c689d4f06ecf1ebc8447
ge4096 <graham@gellinghausen.com>
2017-11-05 23:15:24 -0600

    Changed binary name, added dependency check to test script

--- asm/test/test.sh
+++ asm/test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TMPFILE=$(mktemp /tmp/asm-test.XXXXXX)
+TMPFILE=$(mktemp /tmp/sfsasm-test.XXXXXX)
 SUCCESS=0
 
 test_command () {
@@ -20,22 +20,30 @@ test_command () {
     fi
 }
 
+for DEP in "diff" "make" "valgrind"; do
+    command -v $DEP > /dev/null
+    if [ ! $? -eq 0 ]; then
+        echo "Could not locate dependency" $DEP
+        SUCCESS=1
+    fi
+done
+
 cd $(dirname "$0")/..
 make clean > /dev/null
 
 test_command "make ARGCFLAGS=-Werror"
 test_command "make test ARGCFLAGS=-Werror"
-test_command "./test/asm-test"
-test_command "valgrind -q --leak-check=full --error-exitcode=1 ./test/asm-test"
+test_command "./test/sfsasm-test"
+test_command "valgrind -q --leak-check=full --error-exitcode=1 \
+./test/sfsasm-test"
 
 rm -f ./test/test_output.s
 
-test_command "./bin/asm ./test/test_input.asm ./test/test_output.s"
-test_command "valgrind -q --leak-check=full --error-exitcode=1 ./bin/asm \
+test_command "./bin/sfsasm ./test/test_input.asm ./test/test_output.s"
+test_command "valgrind -q --leak-check=full --error-exitcode=1 ./bin/sfsasm \
 ./test/test_input.asm ./test/test_output.s"
 test_command "diff <(hexdump -C ./test/test_output.s) <(hexdump -C \
 ./test/test_expected_output.s)"
-
 
 if [ $SUCCESS -eq 0 ]; then
     echo
